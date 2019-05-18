e783c44329347606a57f3b18b33187b89fd3c2b2
alvaroximenesrj <alvaroximenesrj@gmail.com>
2019-05-18 19:40:56 -0300

    update

--- Projeto.API/Startup.cs
+++ Projeto.API/Startup.cs
@@ -41,7 +41,7 @@ namespace Projeto.API
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
 
